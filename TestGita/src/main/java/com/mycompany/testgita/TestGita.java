ee871ef9313f2436d6360463927d999ce147fd2e
kapema <145340839+kapema@users.noreply.github.com>
2024-09-17 15:52:05 +0200

    to jest komętarz, dodaje test gita ( projekt do bazy)

---
+++ TestGita/src/main/java/com/mycompany/testgita/TestGita.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testgita;
+
+/**
+ *
+ * @author egzamin
+ */
+public class TestGita {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
