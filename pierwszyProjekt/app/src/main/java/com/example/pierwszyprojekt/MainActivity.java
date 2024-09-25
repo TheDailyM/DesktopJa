2275cbb606f7ec27b716aadb8ef3ed5c7aca12b1
kapema <145340839+kapema@users.noreply.github.com>
2024-09-25 15:11:25 +0200

    juz jest troche dzialan

--- pierwszyProjekt/app/src/main/java/com/example/pierwszyprojekt/MainActivity.java
+++ pierwszyProjekt/app/src/main/java/com/example/pierwszyprojekt/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pierwszyprojekt;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
     Button przycisk7;
     Button przycisk8;
     Button przycisk9;
+    TextView wyswietlacz;
+    Button dodawanie;
+    int pierwsza_liczba;
+    int druga_liczba;
+    int wynik;
+    char dzialanie;
+    Button oblicz_wynik;
+    Button odejmowanie;
+    Button mnozenie;
+    Button dzielenie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,37 +53,99 @@ public class MainActivity extends AppCompatActivity {
         przycisk7 = findViewById(R.id.button_7);
         przycisk8 = findViewById(R.id.button_8);
         przycisk9 = findViewById(R.id.button_9);
-
+        wyswietlacz = findViewById(R.id.wyswietlacz);
+        dodawanie = findViewById(R.id.button_addition);
+        oblicz_wynik = findViewById(R.id.button_result);
+        dodawanie = findViewById(R.id.button_subtraction);
+        mnozenie = findViewById(R.id.button_multiplication);
+        dzielenie = findViewById(R.id.button_division);
 
         przycisk0.setOnClickListener(E ->{
-            System.out.println("0");
+            wyswietlacz.setText(wyswietlacz.getText()+"0");
         });
         przycisk1.setOnClickListener(E ->{
-            System.out.println("1");
+            wyswietlacz.setText(wyswietlacz.getText()+"1");
         });
         przycisk2.setOnClickListener(E ->{
-            System.out.println("2");
+            wyswietlacz.setText(wyswietlacz.getText()+"2");
         });
         przycisk3.setOnClickListener(E ->{
-            System.out.println("3");
+            wyswietlacz.setText(wyswietlacz.getText()+"3");
         });
         przycisk4.setOnClickListener(E ->{
-            System.out.println("4");
+            wyswietlacz.setText(wyswietlacz.getText()+"4");
         });
         przycisk5.setOnClickListener(E ->{
-            System.out.println("5");
+            wyswietlacz.setText(wyswietlacz.getText()+"5");
         });
         przycisk6.setOnClickListener(E ->{
-            System.out.println("6");
+            wyswietlacz.setText(wyswietlacz.getText()+"6");
         });
         przycisk7.setOnClickListener(E ->{
-            System.out.println("7");
+            wyswietlacz.setText(wyswietlacz.getText()+"7");
         });
         przycisk8.setOnClickListener(E ->{
-            System.out.println("8");
+            wyswietlacz.setText(wyswietlacz.getText()+"8");
         });
         przycisk9.setOnClickListener(E ->{
-            System.out.println("9");
+            wyswietlacz.setText(wyswietlacz.getText()+"9");
+        });
+        dodawanie.setOnClickListener(E ->{
+            if (pierwsza_liczba == 0) {
+                pierwsza_liczba = Integer.getInteger(wyswietlacz.getText().toString());
+            }
+            else {
+                druga_liczba = Integer.getInteger(wyswietlacz.getText().toString());
+            }
+            dzialanie = '+';
+
+        });
+        oblicz_wynik.setOnClickListener(E ->{
+            druga_liczba = Integer.getInteger(wyswietlacz.getText().toString());
+            switch (dzialanie){
+                case '+':
+                    wynik = pierwsza_liczba + druga_liczba;
+                case '-':
+                    wynik = pierwsza_liczba - druga_liczba;
+                case '*':
+                    wynik = pierwsza_liczba * druga_liczba;
+                case '/':
+                    if (druga_liczba != 0){
+                        wynik = pierwsza_liczba / druga_liczba;
+                    }
+                    else {
+                        wyswietlacz.setText("BŁĄD");
+                    }
+            }
+            wyswietlacz.setText(String.valueOf(wynik));
+            pierwsza_liczba = wynik;
+        });
+        odejmowanie.setOnClickListener(E ->{
+            if (pierwsza_liczba == 0 ){
+                pierwsza_liczba = Integer.getInteger(wyswietlacz.getText().toString());
+            }
+            else {
+                druga_liczba = Integer.getInteger(wyswietlacz.getText().toString());
+            }
+            dzialanie = '-';
+        });
+        mnozenie.setOnClickListener(E ->{
+            if (pierwsza_liczba == 0){
+                pierwsza_liczba = Integer.getInteger(wyswietlacz.getText().toString());
+            }
+            else {
+                druga_liczba = Integer.getInteger(wyswietlacz.getText().toString());
+            }
+            dzialanie = '*';
+        });
+        dzielenie.setOnClickListener(E ->{
+            if (pierwsza_liczba == 0){
+                pierwsza_liczba = Integer.getInteger(wyswietlacz.getText().toString());
+            }
+            else {
+                druga_liczba = Integer.getInteger(wyswietlacz.getText().toString());
+            }
+            dzialanie = '/';
         });
 
     }
